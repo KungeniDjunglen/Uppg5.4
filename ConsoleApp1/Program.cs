c39f628ee25037dd26399b2bdb59026d17739853
Admin <Admin@TEB127118>
2023-01-18 11:52:09 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace uppg
+{
+    class Uppgift
+    {
+        static void Main(string[] args)
+        {
+            int[] years = { 5, 1648, 1939, 1648, 420, 2001, 2022 };
+            List<int> SameYear = new List<int>();
+            Console.WriteLine("Skriv in året du vill jämföra ");
+            int SelectedYear = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < years.Length; i++)
+            {
+                if (years[i] == SelectedYear)
+                {
+                    SameYear.Add(i);
+                }
+            }
+            if (SameYear.Count < 0)
+            {
+                foreach(int year in SameYear)
+                {
+                    Console.WriteLine(year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nummret kunde inte hittas");
+            }
+
+        }
+    }
+}
